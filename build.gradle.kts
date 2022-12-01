3a964934638445c5b6c03fe86bc1cbfab2ef1552
Florian Cramer <florian.cramer@web.de>
2022-12-01 17:39:41 +0100

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,31 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.7.22"
+
+    id("io.gitlab.arturbosch.detekt") version "1.22.0"
+    id("org.jlleitschuh.gradle.ktlint") version "11.0.0"
+}
+
+group = "de.cramer"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "17"
+}
+
+detekt {
+    config = files("$projectDir/.config/detekt.yml")
+}
