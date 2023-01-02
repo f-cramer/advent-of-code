1ef0fc8bda648f5fca8fab4eca0a816f0f072acd
Florian Cramer <florian.cramer@web.de>
2023-01-02 07:45:51 +0100

    Solution for Day 11 - 2021

---
+++ src/main/kotlin/de/cramer/adventofcode/year2021/day11/Day11.kt
@@ -0,0 +1,89 @@
+package de.cramer.adventofcode.year2021.day11
+
+import de.cramer.adventofcode.Vector
+import de.cramer.adventofcode.checkTestResult
+import de.cramer.adventofcode.get
+import de.cramer.adventofcode.isValidIndex
+import de.cramer.adventofcode.readInput
+import de.cramer.adventofcode.readTestInput
+import de.cramer.adventofcode.runProblem01
+import de.cramer.adventofcode.runProblem02
+import de.cramer.adventofcode.set
+import de.cramer.adventofcode.vectors
+
+fun main() {
+    val testInput = readTestInput().parse()
+    val testResult01 = problem01(testInput)
+    checkTestResult(testResult01, 1656)
+    val testResult02 = problem02(testInput)
+    checkTestResult(testResult02, 195)
+
+    val input = readInput().parse()
+    runProblem01 { problem01(input) }
+    runProblem02 { problem02(input) }
+}
+
+private fun problem01(input: List<List<Int>>): Int {
+    val octopuses = input.map { it.toMutableList() }
+    var flashes = 0
+    repeat(100) {
+        val flashers = octopuses.simulate()
+        flashes += flashers.size
+    }
+
+    return flashes
+}
+
+private fun problem02(input: List<List<Int>>): Int {
+    val octopuses = input.map { it.toMutableList() }
+    val totalOctopuses = octopuses.sumOf { it.size }
+    var round = 0
+    while (true) {
+        round++
+
+        val flashers = octopuses.simulate()
+        if (flashers.size == totalOctopuses) {
+            return round
+        }
+    }
+}
+
+private fun String.parse(): List<List<Int>> {
+    return lineSequence()
+        .map { l -> l.map { it.digitToInt() } }
+        .toList()
+}
+
+private fun List<MutableList<Int>>.simulate(): Set<Vector> {
+    vectors().forEach { this[it] += 1 }
+    val flashers = mutableSetOf<Vector>()
+    while (true) {
+        val newFlashers = vectors()
+            .filterNot { it in flashers }
+            .filter { this[it] > 9 }
+            .toList()
+        if (newFlashers.isEmpty()) {
+            break
+        }
+
+        flashers += newFlashers
+        newFlashers.asSequence()
+            .flatMap { getNeighbors(it) }
+            .forEach { this[it] += 1 }
+    }
+
+    flashers.forEach { this[it] = 0 }
+    return flashers
+}
+
+private operator fun List<List<Int>>.contains(value: Int) = any { value in it }
+
+private val neighborOffsets = listOf(
+    Vector(-1, -1), Vector(0, -1), Vector(1, -1),
+    Vector(-1, 0), Vector(1, 0),
+    Vector(-1, 1), Vector(0, 1), Vector(1, 1),
+)
+
+private fun List<List<Int>>.getNeighbors(position: Vector) = neighborOffsets.asSequence()
+    .map { position + it }
+    .filter { isValidIndex(it) }
