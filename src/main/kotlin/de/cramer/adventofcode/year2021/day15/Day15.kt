7d67556ecf40b96c560b6e2e6ffac9d6d0987930
Florian Cramer <florian.cramer@web.de>
2023-01-03 18:31:23 +0100

    Solution for Day 15 - 2021

---
+++ src/main/kotlin/de/cramer/adventofcode/year2021/day15/Day15.kt
@@ -0,0 +1,106 @@
+package de.cramer.adventofcode.year2021.day15
+
+import de.cramer.adventofcode.Vector
+import de.cramer.adventofcode.checkTestResult
+import de.cramer.adventofcode.get
+import de.cramer.adventofcode.isValidIndex
+import de.cramer.adventofcode.readInput
+import de.cramer.adventofcode.readTestInput
+import de.cramer.adventofcode.runProblem01
+import de.cramer.adventofcode.runProblem02
+import java.util.PriorityQueue
+import kotlin.math.abs
+
+fun main() {
+    val testInput = readTestInput().parse()
+    val testResult01 = problem01(testInput)
+    checkTestResult(testResult01, 40)
+    val testResult02 = problem02(testInput)
+    checkTestResult(testResult02, 315)
+
+    val input = readInput().parse()
+    runProblem01 { problem01(input) }
+    runProblem02 { problem02(input) }
+}
+
+private fun problem01(input: List<List<Int>>): Int {
+    val map = HeightMap(input, Vector(0, 0), Vector(input[0].lastIndex, input.lastIndex))
+    return map.findShortestPathLength()
+}
+
+private fun problem02(input: List<List<Int>>): Int {
+    val topLeft = Vector(0, 0)
+    val yLength = input.size
+
+    val grid = (0 until yLength * 5).map { y ->
+        val yField = y / yLength
+        val xLength = input[y % yLength].size
+        (0 until xLength * 5).map { x ->
+            val vectorInTopLeft = Vector(x % xLength, y % yLength)
+            val topLeftValue = input[vectorInTopLeft]
+
+            val fieldVector = Vector(x / xLength, yField)
+            val distance = topLeft.getDistanceTo(fieldVector)
+            val newValue = (topLeftValue + distance) % 10
+            if (newValue < topLeftValue) newValue + 1 else newValue
+        }
+    }
+    val map = HeightMap(grid, Vector(0, 0), Vector(grid[0].lastIndex, grid.lastIndex))
+    return map.findShortestPathLength()
+}
+
+private fun String.parse(): List<List<Int>> {
+    return lineSequence()
+        .map { l -> l.map { it.digitToInt() } }
+        .toList()
+}
+
+private data class HeightMap(
+    val grid: List<List<Int>>,
+    val start: Vector,
+    val end: Vector,
+) {
+    operator fun get(vector: Vector) = grid[vector]
+
+    fun getNeighbors(vector: Vector) = sequenceOf(
+        Vector(vector.x - 1, vector.y), Vector(vector.x + 1, vector.y),
+        Vector(vector.x, vector.y - 1), Vector(vector.x, vector.y + 1),
+    )
+        .filter { grid.isValidIndex(it) }
+}
+
+private fun HeightMap.findShortestPathLength(): Int {
+    data class VectorWithDistance(val vector: Vector, val distance: Int)
+
+    val priorityQueue = PriorityQueue<VectorWithDistance>(compareBy { it.distance })
+    val visited = mutableSetOf<Vector>()
+    val totalRiskLevel = mutableMapOf<Vector, Int>()
+
+    totalRiskLevel[start] = 0
+    priorityQueue += VectorWithDistance(start, 0)
+
+    while (priorityQueue.isNotEmpty()) {
+        val (location, distance) = priorityQueue.remove()
+        if (location == end) break
+        visited += location
+
+        val previousRiskLevel = totalRiskLevel[location]
+        if (previousRiskLevel == null || previousRiskLevel < distance) continue
+
+        for (neighbors in getNeighbors(location).filterNot { it in visited }) {
+            val newRiskLevel = previousRiskLevel + this[neighbors]
+            if (newRiskLevel < totalRiskLevel.getOrDefault(neighbors, Int.MAX_VALUE)) {
+                totalRiskLevel[neighbors] = newRiskLevel
+                priorityQueue += VectorWithDistance(neighbors, newRiskLevel)
+            }
+        }
+    }
+
+    return totalRiskLevel[end]!!
+}
+
+private fun Vector.getDistanceTo(other: Vector): Int {
+    val dx = abs(x - other.x)
+    val dy = abs(y - other.y)
+    return dx + dy
+}
