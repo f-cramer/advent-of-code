f92d4b463282b5ad3e288e6e6f590d42b136423c
Florian Cramer <florian.cramer@web.de>
2023-01-07 01:46:06 +0100

    Solution for Day 2 - 2020

---
+++ src/main/kotlin/de/cramer/adventofcode/year2020/day2/Day2.kt
@@ -0,0 +1,48 @@
+package de.cramer.adventofcode.year2020.day2
+
+import de.cramer.adventofcode.checkTestResult
+import de.cramer.adventofcode.readInput
+import de.cramer.adventofcode.readTestInput
+import de.cramer.adventofcode.runProblem01
+import de.cramer.adventofcode.runProblem02
+
+fun main() {
+    val testInput = readTestInput().parse()
+    val testResult01 = problem01(testInput)
+    checkTestResult(testResult01, 2)
+    val testResult02 = problem02(testInput)
+    checkTestResult(testResult02, 1)
+
+    val input = readInput().parse()
+    runProblem01 { problem01(input) }
+    runProblem02 { problem02(input) }
+}
+
+private fun problem01(input: List<Input>): Int {
+    return input.count {
+        it.password.count { c -> c == it.policyChar } in it.policyRange
+    }
+}
+
+private fun problem02(input: List<Input>): Int {
+    return input.count {
+        (it.password[it.policyRange.first - 1] == it.policyChar) xor (it.password[it.policyRange.last - 1] == it.policyChar)
+    }
+}
+
+private fun String.parse(): List<Input> {
+    fun String.toInput(): Input {
+        val (rangeStart, rangeEnd, char, password) = split(": ", " ", "-")
+        return Input(password, char.single(), rangeStart.toInt()..rangeEnd.toInt())
+    }
+
+    return lineSequence()
+        .map { it.toInput() }
+        .toList()
+}
+
+private data class Input(
+    val password: String,
+    val policyChar: Char,
+    val policyRange: IntRange,
+)
