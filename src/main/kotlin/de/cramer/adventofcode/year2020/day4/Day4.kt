8f6495f1d10039d3e97382d9bee06c184c657b68
Florian Cramer <florian.cramer@web.de>
2023-01-08 15:25:20 +0100

    Solution for Day 4 - 2020

---
+++ src/main/kotlin/de/cramer/adventofcode/year2020/day4/Day4.kt
@@ -0,0 +1,79 @@
+package de.cramer.adventofcode.year2020.day4
+
+import de.cramer.adventofcode.checkTestResult
+import de.cramer.adventofcode.readInput
+import de.cramer.adventofcode.readTestInput
+import de.cramer.adventofcode.runProblem01
+import de.cramer.adventofcode.runProblem02
+import de.cramer.adventofcode.splitByEmptyLines
+
+fun main() {
+    val testInput = readTestInput().parse()
+    val testResult01 = problem01(testInput)
+    checkTestResult(testResult01, 2)
+    val testResult02 = problem02(testInput)
+    checkTestResult(testResult02, 2)
+
+    val input = readInput().parse()
+    runProblem01 { problem01(input) }
+    runProblem02 { problem02(input) }
+}
+
+private fun problem01(input: List<Map<String, String>>): Int {
+    return input.count {
+        validationData.keys.all { f -> f in it.keys }
+    }
+}
+
+private fun problem02(input: List<Map<String, String>>): Int {
+    return input.count {
+        validationData.all { (field, validator) ->
+            val value = it[field]
+            value != null && validator(value)
+        }
+    }
+}
+
+private fun String.parse(): List<Map<String, String>> {
+    fun String.toPassword() = lineSequence()
+        .flatMap { it.split(" ") }
+        .map { it.split(":") }
+        .map { (a, b) -> a to b }
+        .toMap()
+
+    return splitByEmptyLines()
+        .map { it.toPassword() }
+}
+
+val eclData = setOf("amb", "blu", "brn", "gry", "grn", "hzl", "oth")
+val validationData = mapOf<String, (String) -> Boolean>(
+    "byr" to {
+        it.toIntOrNull() in 1920..2002
+    },
+    "iyr" to {
+        it.toIntOrNull() in 2010..2020
+    },
+    "eyr" to {
+        it.toIntOrNull() in 2020..2030
+    },
+    "hgt" to {
+        if (it.endsWith("cm")) {
+            val number = it.substringBefore("cm").toIntOrNull()
+            number in 150..193
+        } else if (it.endsWith("in")) {
+            val number = it.substringBefore("in").toIntOrNull()
+            number in 59..76
+        } else {
+            false
+        }
+    },
+    "hcl" to {
+        it.startsWith("#") && it.substring(1).matches("""[\da-f]{6}""".toRegex())
+    },
+    "ecl" to {
+        it in eclData
+    },
+    "pid" to {
+        it.length == 9 && it.matches("""\d+""".toRegex())
+    },
+)
