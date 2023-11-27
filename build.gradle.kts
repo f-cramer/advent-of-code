dde4eeb391cb8e1789d3e45acb9f6057dab38b08
Florian Cramer <florian.cramer.112@gmail.com>
2023-11-27 14:10:38 +0100

    Bump io.gitlab.arturbosch.detekt from 1.23.3 to 1.23.4

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     kotlin("jvm") version "1.9.21"
 
-    id("io.gitlab.arturbosch.detekt") version "1.23.3"
+    id("io.gitlab.arturbosch.detekt") version "1.23.4"
     id("org.jlleitschuh.gradle.ktlint") version "11.6.1"
 }
 
