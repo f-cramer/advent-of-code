4db217554f9320b0e2c457ce8ab2028d9c85048c
Florian Cramer <florian.cramer.112@gmail.com>
2024-12-22 17:28:03 +0100

    Solution for Day 15 - 2023

---
+++ src/main/kotlin/de/cramer/adventofcode/year2023/day15/Day15.kt
@@ -0,0 +1,73 @@
+package de.cramer.adventofcode.year2023.day15
+
+import de.cramer.adventofcode.checkTestResult
+import de.cramer.adventofcode.readInput
+import de.cramer.adventofcode.readTestInput
+import de.cramer.adventofcode.runProblem01
+import de.cramer.adventofcode.runProblem02
+
+fun main() {
+    val testInput = readTestInput().parse()
+    val testResult01 = problem01(testInput)
+    checkTestResult(testResult01, 1320)
+    val testResult02 = problem02(testInput)
+    checkTestResult(testResult02, 145)
+
+    val input = readInput().parse()
+    runProblem01 { problem01(input) }
+    runProblem02 { problem02(input) }
+}
+
+private fun problem01(input: List<String>): Int {
+    return input.sumOf { it.hash() }
+}
+
+private fun problem02(input: List<String>): Int {
+    data class Lens(val label: String, var focalLength: Int)
+
+    val boxes = Array(256) { mutableListOf<Lens>() }
+
+    input.forEach { instruction ->
+        val (label, focalLengthString) = if ("=" in instruction) {
+            instruction.split("=")
+        } else {
+            listOf(instruction.dropLast(1), "")
+        }
+
+        val boxIndex = label.hash()
+        val box = boxes[boxIndex]
+
+        if (focalLengthString.isEmpty()) {
+            box.removeIf { it.label == label }
+        } else {
+            val focalLength = focalLengthString.toInt()
+            val existingLens = box.firstOrNull { it.label == label }
+            if (existingLens == null) {
+                box += Lens(label, focalLength)
+            } else {
+                existingLens.focalLength = focalLength
+            }
+        }
+    }
+
+    return boxes.asSequence()
+        .mapIndexed { boxIndex, box ->
+            box.mapIndexed { lensIndex, lens ->
+                (boxIndex + 1) * (lensIndex + 1) * lens.focalLength
+            }.sum()
+        }.sum()
+}
+
+private fun String.hash(): Int {
+    var result = 0
+    for (c in this) {
+        result += c.code
+        result *= 17
+        result %= 256
+    }
+    return result
+}
+
+private fun String.parse(): List<String> {
+    return split(",")
+}
